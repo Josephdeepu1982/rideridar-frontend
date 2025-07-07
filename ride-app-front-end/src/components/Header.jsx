1314451a5f08563ad203fa2419d8fcc72aadc285
Josephdeepu <josephdeepu1982@gmail.com>
2025-07-07 21:11:58 +0800

    Created Header.jsx and Header.css

--- ride-app-front-end/src/components/Header.jsx
+++ ride-app-front-end/src/components/Header.jsx
@@ -1,10 +1,21 @@
-const Header = (props) => {
-  console.log(props);
+import React from "react";
+import { Link } from "react-router-dom";
+import "../CSS/Header.css";
 
+const Header = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <header className="header">
+      <div className="container">
+        <div className="logo">
+          <Link to="/">Rideridar</Link>
+        </div>
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/booking">Book Now</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+      </div>
+    </header>
   );
 };
 

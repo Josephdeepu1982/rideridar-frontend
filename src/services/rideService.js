c349318e9846414a76602d86390cbc6cdf34109f
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-07-19 12:19:10 +0800

    copied Joseph code for rideService

--- src/services/rideService.js
+++ src/services/rideService.js
@@ -1,14 +1,27 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/book`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/booking`;
 
 const index = async () => {
-  try {
-    const res = await fetch(BASE_URL);
-    return res.json();
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const res = await fetch(BASE_URL); //GET request
+        return res.json(); //retruns JSON response
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const createBooking = async (bookingData) => {
+    try {
+        const res = await fetch(BASE_URL, {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(bookingData),
+        });
+        return res.json();
+    } catch (error) {
+        console.error("Error Creating Booking", error);
+    }
 };
 
 console.log(await index());
 
-export { index };
+export { index, createBooking };

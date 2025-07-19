eb990cbe367a54cc0d964a6b560dca7fc2870260
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-07-19 12:17:17 +0800

    minor file arrangement and import updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../CSS/Header.css";
+import "../assets/css/booking/Header.css";
 
 const Header = () => {
     return (
         <header className="header">
             <div className="container">
                 <div className="logo">
-                    <Link to="/">Rideridar</Link>
+                    <Link to="/">Ridevanta</Link>
                 </div>
-                <nav className="nav">
-                    <Link to="/">Home</Link>
-                    <Link to="/booking">Book Now</Link>
-                    <Link to="/login">Login</Link>
-                    <Link to="/dashboard">Dashboard</Link>
-                </nav>
             </div>
         </header>
     );

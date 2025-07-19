f47ee31dccbaa4e4ab49a263a128b48e9ff3583a
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-07-19 11:25:50 +0800

    display header and footer in all pages except dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import LandingPage from "./components/LandingPage";
-import BookingForm from "./components/BookingForm";
-import Confirmation from "./components/Confirmation";
+import Footer from "./components/booking/Footer";
+import LandingPage from "./components/booking/LandingPage";
+import Confirmation from "./components/booking/Confirmation";
+// import TabBookingForm from "./components/booking/TabBookingForm";
+// import CarSelection from "./components/booking/CarSelection";
+// import BookingDetails from "./components/booking/BookingDetails";
+// import BookingReview from "./components/booking/BookingReview";
 import Dashboard from "./components/dashboard/Dashboard";
+
+const Layout = ({ children }) => {
+    const location = useLocation();
+
+    // don't show header and footer on the dashboard
+    const hideHeaderFooter = location.pathname === "/dashboard";
+
+    return (
+        <div>
+            {!hideHeaderFooter && <Header />}
+            {children}
+            {!hideHeaderFooter && <Footer />}
+        </div>
+    );
+};
 
 const App = () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    <Route path="/booking" element={<BookingForm />} />
-                    <Route path="/confirmation" element={<Confirmation />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                </Routes>
-                <Footer />
-            </div>
+            <Layout>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                        <Route
+                            path="/confirmation"
+                            element={<Confirmation />}
+                        />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                    </Routes>
+                </div>
+            </Layout>
         </BrowserRouter>
     );
 };

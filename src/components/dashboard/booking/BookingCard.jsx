98a2a97026a93e694106955d484d4d9c0b4ab74d
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-07-20 15:17:12 +0800

    updated console log error on p having children similar to driverCard

--- src/components/dashboard/booking/BookingCard.jsx
+++ src/components/dashboard/booking/BookingCard.jsx
@@ -38,46 +38,40 @@ const BookingCard = ({ data, index }) => {
                 </HStack>
             </Card.Header>
             <Card.Body>
-                <Card.Description>
-                    <Stack>
+                <Stack>
+                    <HStack>
+                        <Icon mr="1" fontSize="16" as={FaRegUserCircle} />
+                        <Text>{data.contact.name}</Text>
+                    </HStack>
+                    <HStack>
+                        <Icon mr="1" fontSize="16" as={FaRegCalendar} />
+                        <Text>
+                            {new Date(data.trip.date).toLocaleDateString()} at{" "}
+                            {data.trip.time}
+                        </Text>
+                    </HStack>
+                    <HStack>
+                        <Icon mr="1" fontSize="16" as={FaMapLocation} />
+                        <Text>From : {data.trip.pickup}</Text>
+                    </HStack>
+                    <HStack>
+                        <Icon mr="1" fontSize="16" as={FaMapLocationDot} />
+                        <Text>To : {data.trip.dropoff}</Text>
+                    </HStack>
+                    <HStack>
+                        <Icon mr="1" fontSize="16" as={FaCar} />
+                        <Text textTransform="capitalize">
+                            {data.trip.vehicleType}
+                        </Text>
+                    </HStack>
+                    {data.driverAssigned && (
                         <HStack>
-                            <Icon mr="1" fontSize="16" as={FaRegUserCircle} />
-                            <Text>{data.contact.name}</Text>
+                            <Icon mr="1" fontSize="16" as={TbSteeringWheel} />
+                            <Text>{data.driverAssigned.name}</Text>
+                            {/* <Text>Peter</Text> */}
                         </HStack>
-                        <HStack>
-                            <Icon mr="1" fontSize="16" as={FaRegCalendar} />
-                            <Text>
-                                {new Date(data.trip.date).toLocaleDateString()}{" "}
-                                at {data.trip.time}
-                            </Text>
-                        </HStack>
-                        <HStack>
-                            <Icon mr="1" fontSize="16" as={FaMapLocation} />
-                            <Text>From : {data.trip.pickup}</Text>
-                        </HStack>
-                        <HStack>
-                            <Icon mr="1" fontSize="16" as={FaMapLocationDot} />
-                            <Text>To : {data.trip.dropoff}</Text>
-                        </HStack>
-                        <HStack>
-                            <Icon mr="1" fontSize="16" as={FaCar} />
-                            <Text textTransform="capitalize">
-                                {data.trip.vehicleType}
-                            </Text>
-                        </HStack>
-                        {data.driverAssigned && (
-                            <HStack>
-                                <Icon
-                                    mr="1"
-                                    fontSize="16"
-                                    as={TbSteeringWheel}
-                                />
-                                <Text>{data.driverAssigned.name}</Text>
-                                {/* <Text>Peter</Text> */}
-                            </HStack>
-                        )}
-                    </Stack>
-                </Card.Description>
+                    )}
+                </Stack>
             </Card.Body>
             <Card.Footer flexDirection="column">
                 <HStack w="100%" colorPalette="blue">

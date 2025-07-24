abe957a30844d8c87978ebe3a2ce9d02265e6737
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-07-24 17:49:31 +0800

    added button to driver form

--- src/components/dashboard/Drivers.jsx
+++ src/components/dashboard/Drivers.jsx
@@ -1,13 +1,85 @@
-import { Heading } from "@chakra-ui/react";
+import { useState } from "react";
+import { Heading, Button, Dialog, Portal, CloseButton } from "@chakra-ui/react";
 import DriverList from "./driver/DriverList";
+import DriverForm from "./driver/DriverForm"; 
 
 const Drivers = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [newDriverData, setNewDriverData] = useState({
+        name: "",
+        phone: "",
+        email: "",
+        vehicle: {
+            plateNumber: "",
+            model: "",
+            vehicleType: "sedan", // default value
+        },
+    });
+
+    const handleAddDriverClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        // Reset form data when closing modal
+        setNewDriverData({
+            name: "",
+            phone: "",
+            email: "",
+            vehicle: {
+                plateNumber: "",
+                model: "",
+                vehicleType: "sedan",
+            },
+        });
+    };
+
     return (
         <>
             <Heading as="h1" textAlign="left" color="white" mb={5}>
                 Drivers List
             </Heading>
-            {/* Additional components or content related to drivers can be added here */}
+
+            {/* Add Driver Button with Modal */}
+            <Dialog.Root
+                open={isModalOpen}
+                onOpenChange={(e) => setIsModalOpen(e.open)}
+            >
+                <Dialog.Trigger asChild>
+                    <Button
+                        onClick={handleAddDriverClick}
+                        colorScheme="blue"
+                        mb={4}
+                    >
+                        Add Driver
+                    </Button>
+                </Dialog.Trigger>
+
+                <Portal>
+                    <Dialog.Backdrop />
+                    <Dialog.Positioner>
+                        <Dialog.Content maxW="600px" color="white">
+                            <Dialog.Header>
+                                <Dialog.Title as="h3">Add New Driver</Dialog.Title>
+                            </Dialog.Header>
+
+                            <Dialog.Body>
+                                <DriverForm
+                                    newDriverData={newDriverData}
+                                    setNewDriverData={setNewDriverData}
+                                    onSuccess={handleCloseModal}
+                                />
+                            </Dialog.Body>
+
+                            <Dialog.CloseTrigger asChild>
+                                <CloseButton size="sm" />
+                            </Dialog.CloseTrigger>
+                        </Dialog.Content>
+                    </Dialog.Positioner>
+                </Portal>
+            </Dialog.Root>
+
             <DriverList />
         </>
     );

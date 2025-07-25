99cafe419578cbd3c62d416bfdf80d29bca58972
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-07-26 00:57:07 +0800

    updated dashboard to login by default

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,12 +1,26 @@
+import { useContext } from "react";
 import { Outlet } from "react-router-dom";
+
+import { UserContext } from "@/contexts/UserContext";
+
 import Navigation from "./Navigation";
+import Login from "./Login";
 
 const Dashboard = () => {
+    const login = true;
+    const { user } = useContext(UserContext);
+
     return (
-        <Navigation>
-            {/* This will render matching child route */}
-            <Outlet />
-        </Navigation>
+        <>
+            {user ? (
+                <Navigation>
+                    {/* This will render matching child route */}
+                    <Outlet />
+                </Navigation>
+            ) : (
+                <Login />
+            )}
+        </>
     );
 };
 

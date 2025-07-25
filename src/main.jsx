e98743031c3aaf0ff0ee9083592db1c1b8d76584
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-07-25 23:22:38 +0800

    created user context for login

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
+import { UserProvider } from "./contexts/UserContext";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Provider>
-            <App />
+            <UserProvider>
+                <App />
+            </UserProvider>
         </Provider>
     </StrictMode>
 );
